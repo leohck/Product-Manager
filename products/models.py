100a1d6ff1c0d320734d98c12363ddb3639d2259
Jonas <jonas_barros@outlook.com>
2019-07-11 11:37:25 -0300

    model Products criada

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Products(models.Model):
+    pd_name = models.CharField(max_length=30)
+    pd_description = models.TextField()
+    pd_price = models.DecimalField(max_digits=6, decimal_places=2)
+    pd_photo = models.ImageField(upload_to='products_photos', null=True, blank=True)
+
+    def __str__(self):
+        return self.pd_name
