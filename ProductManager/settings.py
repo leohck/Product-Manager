4b3b2c480d6f26428852ab32b0a06cec5d1a106e
Jonas <jonas_barros@outlook.com>
2019-07-11 16:35:20 -0300

    Implementada página de Login e navbar

--- ProductManager/settings.py
+++ ProductManager/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products.apps.ProductsConfig',
+    'bootstrapform',
 ]
 
 MIDDLEWARE = [
@@ -117,4 +118,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "products/static/css"),
+]
